7c7b05f0757cff1f9d925f1cd47f0f4043e15527
imisiB <imisibailey@gmail.com>
2026-08-06 14:56:16 +0100

    Created a time table app

--- script.js
+++ script.js
@@ -12,50 +12,57 @@ for (let i = 0; i < 12; i++) {
 }
 
 let cells = document.querySelectorAll("td");
-  for (let i = 0; i < 97; i++) {
-    cells.forEach(cell => {
-      cell.setAttribute("cell", `${i}`);
-      cell.innerText = localStorage.getItem(`cell-${i}`);
-    });
-    // let cell = document.createElement("td");
-    // cells[i].innerText = localStorage.getItem(`cell-${i}`);
-    // console.log("cell id", cells[i].getAttribute("cell"), "cell value", cells[i].innerHTML);
+let clearButton = document.querySelector('.clear')
 
-    cells[i].addEventListener("click", () => {
-      document.querySelector(".modal-div").classList.add("modalAfter");
-      modalInput = document.querySelector(".modal-input");
-      modalInput.value = localStorage.getItem(`cell-${i}`);
-    });
-
-
-
-
-    let saveButton = document.querySelector(".save-button");
-    saveButton.addEventListener("click", () => {
-      document.querySelector(".modal-div").classList.remove("modalAfter");
-      modalInput = document.querySelector(".modal-input");
-      localStorage.setItem(`cell-${cells[i].getAttribute("table-id")}`, modalInput.value);
-      cells[i].innerText = localStorage.getItem(`cell-${cells[i].getAttribute("table-id")}`) || `cell-${i}`;
-
-    });
-    window.addEventListener('load', () => {
-      localStorage.getItem(`cell${i}`) = cells[i].innerText;
-    })
-}
-
-let buttons = document.querySelectorAll(".button");
-buttons.forEach(button => {
-  button.addEventListener("click", () => {
-    document.querySelector(".modal-div").classList.add("modalAfter");
+clearButton.addEventListener('click',() => {
+  localStorage.clear();
+  cells.forEach((cell) => {
+    cell.querySelector('button').innerText = '';
   });
 });
+let saveButton = document.querySelector(".save-button");
+let modalDiv = document.querySelector('.modal-div')
+let modalInput = document.querySelector(".modal-input");
+let activeCellIndex = null;
 
-// let saveButton = document.querySelector(".save-button");
-// saveButton.addEventListener("click", () => {
-//   document.querySelector(".modal-div").classList.remove("modalAfter");
-//   modalInput = document.querySelector(".modal-input");
-//   localStorage.setItem("cell-96", modalInput.value);
-// });
+cells.forEach((cell, index) => {
+  cell.setAttribute('index', index)
+
+  let btn = cell.querySelector('.button')
+
+  let savedValue = localStorage.getItem(`cell-${index}`) || ''
+
+  btn.innerText = savedValue
+
+  
+  btn.addEventListener('click', () => {
+    activeCellIndex = index;
+    modalInput.value = localStorage.getItem(`cell-${index}`) || ''
+    modalDiv.classList.add('modalAfter')
+  })
+
+
+
+});
+
+saveButton.addEventListener("click", () => {
+  if(activeCellIndex != null) {
+    let newValue = modalInput.value
+
+    localStorage.setItem(`cell-${activeCellIndex}`, newValue)
+  
+  let activeCell = document.querySelector(`td[index='${activeCellIndex}']`);
+  if (activeCell) {
+    activeCell.querySelector('.button').innerText = newValue;
+  }}
+
+  modalDiv.classList.remove('modalAfter')
+  activeCellIndex = null
+});
+  
+
+
+
 let cancelButton = document.querySelector(".cancel-button");
 cancelButton.addEventListener("click", () => {
   document.querySelector(".modal-div").classList.remove("modalAfter");
